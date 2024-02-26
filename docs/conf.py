a807999f5113b48b876e1ff47cedcf3210fbdad2
Kayce Basques <kaycebasques@gmail.com>
2024-02-25 20:54:07 -0800

    init

---
+++ docs/conf.py
@@ -0,0 +1,14 @@
+project = 'sphinx-theme'
+copyright = '<YEAR>, <NAME>'
+author = '<NAME>'
+release = '0.0.1'
+extensions = [
+    'sphinx-theme'
+]
+templates_path = ['_templates']
+exclude_patterns = [
+    '_build',
+    'build.sh',
+    'venv', 
+]
+html_theme = 'sphinx-theme'
