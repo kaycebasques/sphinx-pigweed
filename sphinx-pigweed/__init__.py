2521bb111e32f97e1da744e41a6f635ef19838b8
Kayce Basques <kayce@google.com>
2024-02-26 21:59:30 -0800

    add searchbox, pagenav, banner stuff

--- sphinx-pigweed/__init__.py
+++ sphinx-pigweed/__init__.py
@@ -1,4 +1,11 @@
 import os
 
+def add_context(app, pagename, templatename, context, doctree):
+    context['pw_pagenav_label'] = app.config.pw_pagenav_label
+    context['pw_banner_text'] = app.config.pw_banner_text
+
 def setup(app):
     app.add_html_theme('sphinx-pigweed', os.path.abspath(os.path.dirname(__file__)))
+    app.add_config_value('pw_pagenav_label', 'Page Contents', 'html', [str])
+    app.add_config_value('pw_banner_text', None, 'html', [str])
+    app.connect('html-page-context', add_context) 
